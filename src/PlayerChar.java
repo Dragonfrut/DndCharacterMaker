e40aa153e719fa9863b554fe29cc6b1f5172bdf8
Mark Ricard <34170972+Dragonfrut@users.noreply.github.com>
2022-12-16 13:37:53 -0500

    creatures now have a max health and current health

--- src/PlayerChar.java
+++ src/PlayerChar.java
@@ -2,8 +2,8 @@ public class PlayerChar extends Creature {
     private int level;
     private String playerClass;
 
-    public PlayerChar(String name, int level, String playerClass, int health, int str, int dex, int con, int intel, int wis, int cha, int str_mod, int dex_mod, int con_mod, int intel_mod, int wis_mod, int cha_mod) {
-        super(name, health, str, dex, con, intel, wis, cha, str_mod, dex_mod, con_mod, intel_mod, wis_mod, cha_mod);
+    public PlayerChar(String name, int level, String playerClass, int maxHealth, int currentHealth, int str, int dex, int con, int intel, int wis, int cha, int str_mod, int dex_mod, int con_mod, int intel_mod, int wis_mod, int cha_mod) {
+        super(name, maxHealth, currentHealth, str, dex, con, intel, wis, cha, str_mod, dex_mod, con_mod, intel_mod, wis_mod, cha_mod);
         this.level = level;
         this.playerClass = playerClass;
     }
@@ -11,8 +11,8 @@ public class PlayerChar extends Creature {
     public void attack(Weapon weapon, Creature target){
         int maxDamage = weapon.getBaseDamage();
         int damageDone = DiceRoller.diceRoll(maxDamage);
-        int initHealth = target.getHealth();
-        target.setHealth(target.getHealth() - damageDone);
+        //int initHealth = target.getCurrentHealth();
+        target.setCurrentHealth(target.getCurrentHealth() - damageDone);
         System.out.println(target.getName() + " has been hit with " + weapon.getName() + " and took " + damageDone + " damage.");
     }
 
